90fa31b6314786c805965782cd5ea1f11030f323
Xinyao Sun <xinyao1@ualberta.ca>
2013-03-06 10:50:52 -0700

    Revert "tiaozhuan"
    This reverts commit 6e4aad302de4fc58c3f5e4719869f6fbee16cae2.

--- CMPUT301W13T05/src/com/example/easycooking/mainpage.java
+++ CMPUT301W13T05/src/com/example/easycooking/mainpage.java
@@ -1,12 +1,8 @@
 package com.example.easycooking;
 
-
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
 
 public class mainpage extends Activity {
 
@@ -14,17 +10,6 @@ public class mainpage extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		 Button b1 = (Button) findViewById(R.id.button1);
-	        b1.setOnClickListener(new Button.OnClickListener() {
-	        	public void onClick(View v) {
-	        		Intent intent = new Intent();
-	        		intent.setClass(MainActivity.this, MyProfile.class);
-	        		//start a add entry activity
-	        		startActivity(intent);
-	        		//close the old activity
-	        		MainActivity.this.finish();
-	        	}
-	        });
 	}
 
 	@Override
@@ -32,11 +17,6 @@ public class mainpage extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
-	}
-	@Override
-	//back button pressed
-	public void onBackPressed() {
-	return;
 	}
 
 }
