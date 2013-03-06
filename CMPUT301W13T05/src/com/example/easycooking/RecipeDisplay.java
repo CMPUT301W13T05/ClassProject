5b5d43c412bae062595c664e2ef33cb46e7f82f7
Xinyao Sun <xinyao1@ualberta.ca>
2013-03-06 10:52:28 -0700

    Revert "Revert "tiaozhuan""
    This reverts commit 90fa31b6314786c805965782cd5ea1f11030f323.

--- CMPUT301W13T05/src/com/example/easycooking/RecipeDisplay.java
+++ CMPUT301W13T05/src/com/example/easycooking/RecipeDisplay.java
@@ -8,23 +8,12 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-public class mainpage extends Activity {
+public class RecipeDisplay extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		 Button b1 = (Button) findViewById(R.id.button1);
-	        b1.setOnClickListener(new Button.OnClickListener() {
-	        	public void onClick(View v) {
-	        		Intent intent = new Intent();
-	        		intent.setClass(MainActivity.this, MyProfile.class);
-	        		//start a add entry activity
-	        		startActivity(intent);
-	        		//close the old activity
-	        		MainActivity.this.finish();
-	        	}
-	        });
+		setContentView(R.layout.result);
 	}
 
 	@Override
