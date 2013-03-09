afe1dd44d72145ef12cc745d82bd8cb55b1102e7
hongzu li <hongzu@ualberta.ca>
2013-03-08 22:15:26 -0700

    database initialize

---
+++ CMPUT301W13T05/src/com/example/easycooking/DatabaseHelper.java
@@ -0,0 +1,65 @@
+package com.example.easycooking;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * SQLiteOpenHelper is a helper class to manage database creation and version management
+ * @author HongZu
+ */
+public class DatabaseHelper extends SQLiteOpenHelper { 
+	private static final int VERSION = 1;
+	private static final String DATABASE = "ecipes.db";
+	/**
+	 * 
+	 * @param context
+	 * @param name
+	 * @param cursorFactory
+	 * @param version
+	 */
+	public DatabaseHelper(Context context) 
+	  { 
+	     super(context, DATABASE, null, VERSION);     
+	     } 
+	
+	//This function is used for first creation of the database
+	@Override
+	public void onCreate(SQLiteDatabase db) {     
+	    // TODO 
+		System.out.println("create a database");
+		//execSQL to execute sql statement
+		db.execSQL("create table localrecipe(" +
+				"rid INTEGER PRIMARY KEY AUTOINCREMENT," +
+				"name TEXT," +
+				"downloaded INTEGER," +
+				"uploaded INTEGER)"); 
+		db.execSQL("create table picture(" +
+				"pid INTEGER PRIMARY KEY AUTOINCREMENT," +
+				"rid INTEGER," +
+				"image blob," +
+				"FOREIGN KEY(rid) REFERENCES localrecipe(rid))");
+		db.execSQL("create table step(" +
+				"sid INTEGER PRIMARY KEY AUTOINCREMENT," +
+				"rid INTEGER," +
+				"steps TEXT," +
+				"FOREIGN KEY(rid) REFERENCES localrecipe(rid))");
+		db.execSQL("create table ingredient(" +
+				"name TEXT," +
+				"rid INTEGER," +
+				"FOREIGN KEY(rid) REFERENCES localrecipe(rid))");
+	}     
+	
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {     
+	    // TODO
+		System.out.println("upgrade the database");
+		db.execSQL("DROP TABLE IF EXISTS localrecipe");
+		db.execSQL("DROP TABLE IF EXISTS picture");
+		db.execSQL("DROP TABLE IF EXISTS step");
+		db.execSQL("DROP TABLE IF EXISTS ingredient");
+		onCreate(db);
+	}          
+}
+
---
+++ CMPUT301W13T05/src/com/example/easycooking/DatabaseManager.java
@@ -0,0 +1,57 @@
+package com.example.easycooking;
+
+import android.view.Menu;
+import android.content.ContentValues;  
+import android.content.Context;
+import android.database.Cursor;  
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;  
+import android.view.View;  
+
+/**
+ * This class manages the database that contains the local recipes.
+ * The local recipes contains download, upload recipes.
+ * All interaction with local recipes much go through the DatabaseManager
+ * @author HongZu
+ *
+ */
+public class DatabaseManager {
+	private static DatabaseManager instance = null;
+
+		private SQLiteDatabase db ;
+		private DatabaseHelper dbHelper;
+		
+		protected DatabaseManager(Context context){
+			dbHelper = new DatabaseHelper(context);
+		}  
+
+		public void open() throws SQLException {
+			db = dbHelper.getWritableDatabase();
+		}
+		public void close(){
+			dbHelper.close();
+		} 
+		/**
+		 * return instance of the database to user
+		 * @param context
+		 * @return
+		 */
+		public static DatabaseManager getInstance(Context context){
+			if(instance == null){
+				instance = new DatabaseManager(context);
+			}
+			return instance; 
+		}
+		
+		//Add recipe
+		public void add_recipe(Recipe recipe) {
+			ContentValues values = new ContentValues();
+			values.put("rid", rid);
+			values.put("name",name);
+			values.put("download", download);
+			values.put("upload", upload);
+			db.insert("localrecipe", null, values);
+
+		}
+
+}
