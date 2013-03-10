627e722567a0dfdfa32102993b81776bcb39ec6e
hongzu li <hongzu@ualberta.ca>
2013-03-10 09:11:02 -0600

    image model

--- CMPUT301W13T05/src/com/example/easycooking/model/Image.java
+++ CMPUT301W13T05/src/com/example/easycooking/model/Image.java
@@ -8,6 +8,25 @@ public class Image implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-
+	private String  IMAGE_ID, image_belongto;
+	public Image(){
+		
+	}
+	public Image(String IMAGE_ID,String image_belongto){
+		super();
+		this.IMAGE_ID = IMAGE_ID;
+		this.image_belongto = image_belongto;			
+	}
+	public String get_IMAGE_ID(){
+		return this.IMAGE_ID;
+	}
+	public void set_IMAGE_ID(String IMAGE_ID){
+		this.IMAGE_ID = IMAGE_ID;
+	}
+	public String get_image_belongto(){
+		return this.image_belongto;
+	}
+	public void set_image_belongto(String image_belongto){
+		this.image_belongto = image_belongto;
+	}
 }
