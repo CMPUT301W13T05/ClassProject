346f34e371007d1f6267f2b8c7c6f0ab65fb4984
hongzu li <hongzu@ualberta.ca>
2013-03-10 10:03:12 -0600

    image model

--- CMPUT301W13T05/src/com/example/easycooking/model/Image.java
+++ CMPUT301W13T05/src/com/example/easycooking/model/Image.java
@@ -1,14 +1,15 @@
 package com.example.easycooking.model;
 
+import java.io.ByteArrayOutputStream;
 import java.io.Serializable;
+
+import android.graphics.Bitmap;
 
 public class Image implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private String  IMAGE_ID, image_belongto;
+	private String  IMAGE_ID,image_belongto;
+	private Bitmap image_bit;
 	public Image(){
 		
 	}
@@ -28,5 +29,15 @@ public class Image implements Serializable{
 	}
 	public void set_image_belongto(String image_belongto){
 		this.image_belongto = image_belongto;
+	}
+	public byte[] get_imageBlob(){
+		image_bit = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+		int size = image_bit.getWidth()*image_bit.getHeight()*4; 
+		ByteArrayOutputStream baos = new ByteArrayOutputStream(size); 
+		byte[] imagedata = baos.toByteArray();
+		return imagedata;
+	}
+	public void set_imageBit(Bitmap image_bit) {
+		this.image_bit = image_bit;
 	}
 }
