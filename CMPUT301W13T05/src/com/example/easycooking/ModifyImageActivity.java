513f0b40e98f464d978198f091313b12243f919f
hongzu li <hongzu@ualberta.ca>
2013-03-11 05:09:51 -0600

    MODIFYIMAGE

--- CMPUT301W13T05/src/com/example/easycooking/ModifyImageActivity.java
+++ CMPUT301W13T05/src/com/example/easycooking/ModifyImageActivity.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import com.example.easycooking.controller.*;
 import com.example.easycooking.model.Image;
@@ -21,21 +22,22 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 import android.app.Activity;
+import android.content.Context;
 //import android.view.Menu;
 import android.content.Intent;
 import android.graphics.Bitmap;
 
 /**
- * setBogoPic() and getPicturePath(Intent intent)
- * are took from project BogoPicGenActivity
+ * setBogoPic() and saveFile() used project BogoPicGenActivity as reference
  * download address https://github.com/abramhindle/BogoPicGen
  * author: Dr.Abram Hindle
  * @author HongZu
  *
  */
 public class ModifyImageActivity extends Activity {
+	private Bitmap ourBMP;
 	private static Recipe mrecipe = new Recipe();
-	private static ArrayList<String> image_list = new ArrayList<String>();
+	private static Image rimages = new Image();
 	private static ArrayList<Image> image_obj_list = new ArrayList<Image>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,55 +59,56 @@ public class ModifyImageActivity extends Activity {
         Button accept = (Button)findViewById(R.id.accept);
         accept.setOnClickListener( new OnClickListener() {
             public void onClick(View v) {
-               //TO DO
+            	saveFile();
             }
+           
         }); 
         Button continue_to = (Button)findViewById(R.id.continue_to);
         continue_to.setOnClickListener( new OnClickListener() {
             public void onClick(View v) {
-               //TO DO
+            	
             }
         }); 
     }
 
 
-    private Bitmap ourBMP;
     private void setBogoPic() {
     	Toast.makeText(this, "Generating Photo", Toast.LENGTH_LONG).show();
     	ImageButton button = (ImageButton)findViewById(R.id.imageButton1);
 		ourBMP = GeneratePhoto.generateBitmap(400, 400);
 		button.setImageBitmap(ourBMP);
 	}
-    private File getPicturePath(Intent intent) {
+   /* private File getPicturePath(Intent intent) {
         Uri uri = (Uri) intent.getExtras().get(MediaStore.EXTRA_OUTPUT);
         return new File(uri.getPath());
-    }	
-    private void processIntent() {
-    	Intent intent = getIntent();
-    	if (intent == null) {
-    		return;
-    	}
-    	try {
-    		if (intent.getExtras() != null) {    
-    			File intentPicture = getPicturePath(intent);
-    			saveBMP(intentPicture, ourBMP);
-    			setResult(RESULT_OK);
-    		} else {
-    			Toast.makeText(this, "Photo Cancelled: No Reciever?", Toast.LENGTH_LONG).show();
-    			setResult(RESULT_CANCELED);
+    }	*/
+    private void saveFile() {
+    	try { 
+    		File file = new File("/sdcard/myImages/");
+    		if(!file.exists()) {
+    			file.mkdirs();
     		}
+    		FileOutputStream fos = null;
+    		String pic_date = Long.toString(System.currentTimeMillis());
+    		String image_uri = "/sdcard/myImages/"+pic_date+".JPEG";
+    		fos = new FileOutputStream(image_uri);
+    		ourBMP.compress(Bitmap.CompressFormat.JPEG, 75, fos);
+    		fos.close();
+    		rimages.set_IMAGE_ID(pic_date);
+    		rimages.set_IMAGE_ID(mrecipe.getID());
+    		rimages.set_imageUri(image_uri);
+    		image_obj_list.add(rimages);
+    		Toast.makeText(this, "Image Saved", Toast.LENGTH_LONG).show();
     	} catch (FileNotFoundException e) {
     		Toast.makeText(this, "Couldn't Find File to Write to?", Toast.LENGTH_LONG).show();
-    		setResult(RESULT_CANCELED);    	
     	} catch (IOException e) {
     		Toast.makeText(this, "Couldn't Write File!", Toast.LENGTH_LONG).show();
-    		setResult(RESULT_CANCELED);
     	}
     	finish();
     }
-    private void saveBMP( File intentPicture, Bitmap ourBMP) throws IOException, FileNotFoundException {
-		OutputStream out = new FileOutputStream(intentPicture);
+    /*private void saveBMP( File FileName, Bitmap ourBMP) throws IOException, FileNotFoundException {
+		OutputStream out = new FileOutputStream(FileName);
 		ourBMP.compress(Bitmap.CompressFormat.JPEG, 75, out);
 		out.close();
-    }
+    }*/
 }
