39b4175976ce2cf29ac13607d393ac930dcea9c3
chenkun <chenkun@ualberta.ca>
2013-03-11 13:54:03 -0600

    Create LocalDBtest.java

---
+++ CMPUT301W13T05/src/com/example/easycooking/test/LocalDBtest.java
@@ -0,0 +1,14 @@
+package com.example.easycooking.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LocalDBtest {
+
+  @Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
