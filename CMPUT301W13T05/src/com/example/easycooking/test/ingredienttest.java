2bbbddaaa09161938b3ef5519571138c91a58e39
chenkun <chenkun@ua18.cs.ualberta.ca>
2013-03-11 02:51:16 -0600

    b

--- CMPUT301W13T05/src/com/example/easycooking/test/ingredienttest.java
+++ CMPUT301W13T05/src/com/example/easycooking/test/ingredienttest.java
@@ -1,9 +1,7 @@
 package com.example.easycooking.test;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
-import java.util.ArrayList;
+import junit.framework.TestCase;
 
 import com.example.easycooking.model.Ingredient;
 import com.example.easycooking.model.Recipe;
@@ -19,7 +17,7 @@ import android.app.Activity;
 import android.content.Intent;
 import com.example.easycooking.ModifyIngredientsActivity;
 
-public class ingredienttest {
+public class ingredienttest extends TestCase {
 
   
        //Test if the ingredient is empty
@@ -51,3 +49,65 @@ public class ingredienttest {
 
 
 	}
+/**
+
+//test remove ingredient	    	 
+	@Test
+	public void testRemoveIngredient()
+	{
+		Recipe recipe = new Recipe();  
+      Ingredient ingredient1 = new Ingredient("test1", (double) 5, "test1");    
+      Ingredient ingredient2 = new Ingredient("test2", (double) 6, "test2");
+      try
+		{
+			recipe.addIngredient(ingredient1);
+			recipe.addIngredient(ingredient2);
+		} catch (DuplicateIngredientException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+      recipe.removeIngredient(ingredient1);
+      assertEquals("Remove Ingredient", ingredient2, recipe.getIngredients().get(0));		
+	}
+
+
+
+	@Test
+	public void testAddIngredient()
+	{
+
+	    Recipe recipe = new Recipe();  
+	    Ingredient ingredient = new Ingredient("test", (double) 5, "test");        
+	    try
+	    {
+	    	recipe.addIngredient(ingredient);
+		} catch (DuplicateIngredientException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        assertEquals("Get Ingredient", ingredient, recipe.getIngredient(ingredient.getType()));	
+	}
+
+	@Test
+	public void testGetOnServer()
+	{
+      Recipe recipe = new Recipe();  
+      recipe.setOnServer(true);  
+      assertEquals("GetOnServer", true , recipe.getOnServer());
+	}
+
+	@Test
+	public void testSetOnServer()
+	{
+      Recipe recipe = new Recipe();  
+      recipe.setOnServer(true);  
+      assertEquals("GetOnServer", true , recipe.getOnServer());
+	}
+
+
+
+/**
+* Tests the equals method
+*/
