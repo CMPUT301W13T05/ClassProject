4cf6f8001deebb042d664da924d96b1978a3bc21
chenkun <chenkun@ualberta.ca>
2013-03-12 13:46:47 -0600

    Create PhotoTest.java

---
+++ CMPUT301W13T05/src/com/example/easycooking/test/PhotoTest.java
@@ -0,0 +1,40 @@
+package com.example.easycooking.test;
+/**
+ * This class is testing the Image model
+ * @author Chenkun
+ *
+ *
+ */
+import com.example.easycooking.model.*;
+
+import org.junit.Test;
+
+public class PhotoTest {
+
+
+	@Test
+	public void testTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPhoto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPhoto() {
+		fail("Not yet implemented");
+	}
+
+}
