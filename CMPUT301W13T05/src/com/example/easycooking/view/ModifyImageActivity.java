07d4d7badb6ce7ec38cae49adcdd65dd013c8607
Xinyao Sun <xinyao1@ualberta.ca>
2013-03-27 00:28:05 -0600

    none

--- CMPUT301W13T05/src/com/example/easycooking/view/ModifyImageActivity.java
+++ CMPUT301W13T05/src/com/example/easycooking/view/ModifyImageActivity.java
@@ -186,7 +186,7 @@ public class ModifyImageActivity extends Activity {
     public String bitmaptoString(Bitmap bitmap) {
         String string = null;
         ByteArrayOutputStream bStream = new ByteArrayOutputStream();
-        bitmap.compress(CompressFormat.PNG, 100, bStream);
+        bitmap.compress(CompressFormat.JPEG, 100, bStream);
         byte[] bytes = bStream.toByteArray();
         string = Base64.encodeToString(bytes, Base64.DEFAULT);
         return string;
