c4cf5003b0791e2728154dadfcbdfcd05670dbd6
hongzu li <hongzu@ualberta.ca>
2013-03-28 07:48:43 -0600

    minor change

---
+++ CMPUT301W13T05/src/com/example/easycooking/controller/UsefulFunctions.java
@@ -0,0 +1,20 @@
+package com.example.easycooking.controller;
+
+import java.util.ArrayList;
+
+import com.example.easycooking.model.*;
+
+/**
+ * This class contains some functions make our life easier.
+ * @author HongZu
+ */
+public class UsefulFunctions {
+	public ArrayList<Recipe> Unique(ArrayList<Recipe> inputList){
+		ArrayList<Recipe> uniqueList= new ArrayList<Recipe>();
+		for(int i = 0;i<inputList.size();i++){
+			
+		}
+		return uniqueList;
+	}
+}
+
