9c924fdf29d87bc405f60253f1a48a8f8dc207e1
hongzu li <hongzu@ualberta.ca>
2013-03-28 03:14:39 -0600

    now we can send recipe via email

--- CMPUT301W13T05/src/com/example/easycooking/controller/EmailController.java
+++ CMPUT301W13T05/src/com/example/easycooking/controller/EmailController.java
@@ -2,9 +2,13 @@ package com.example.easycooking.controller;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -20,30 +24,37 @@ import com.google.gson.Gson;
 public class EmailController {
 	private Gson gson = new Gson();
 	Uri jsonFileUri;
-    public void sentRecipe(Recipe recipe){
+	public Intent sentRecipe(Recipe recipe) throws IOException{
     	String Json = gson.toJson(recipe);
     	Intent intent = new Intent(Intent.ACTION_SEND);
+    	
     	String folder = Environment.getExternalStorageDirectory().getAbsolutePath() + "/sendrecipes";
         File folderF = new File(folder);
         if (!folderF.exists()) {
             folderF.mkdir();
         }
-        String jsonFilePath = folder + "/" + String.valueOf(System.currentTimeMillis())+".json";
+       
+        String jsonFilePath = folder + "/" +recipe.getName()+".json";
 		File jsonFile = new File(jsonFilePath);
+		FileWriter fw=new FileWriter(jsonFile);
+	    BufferedWriter buffw=new BufferedWriter(fw);
+	    PrintWriter pw=new PrintWriter(buffw);
+	    fw.flush();
+	    pw.write(Json);
+	    pw.close();
+	    buffw.close();
+	    fw.close();
+	    
 		jsonFileUri = Uri.fromFile(jsonFile);
-		intent.putExtra(Intent.EXTRA_STREAM, jsonFileUri.getPath());
-		intent.setType("application/json");
-		Intent.createChooser(intent, "Choose Email Client");
-		startActivity(intent);
-
-    }
-    private void startActivity(Intent intent) {
-		// TODO Auto-generated method stub
+		System.out.println(jsonFileUri.getPath());
 		
-	}
-	public void attachImages(Image image){
-    	
-    }
+		intent.putExtra(Intent.EXTRA_STREAM, jsonFileUri);
+		intent.putExtra(Intent.EXTRA_SUBJECT, recipe.getName());
+		intent.putExtra(Intent.EXTRA_TEXT   , "");
+		intent.setType("application/text");
+		Intent.createChooser(intent, "Choose Email Client");
+		return intent;
+    } 
     public void downloadFromEmail(){
     	
     }
