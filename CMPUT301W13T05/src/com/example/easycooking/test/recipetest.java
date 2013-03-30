ec5a5cab88d9b28cda4cd5461e1118efb04bd575
chenkun <chenkun@ualberta.ca>
2013-03-29 19:19:47 -0600

    Update recipetest.java

--- CMPUT301W13T05/src/com/example/easycooking/test/recipetest.java
+++ CMPUT301W13T05/src/com/example/easycooking/test/recipetest.java
@@ -6,116 +6,81 @@ package com.example.easycooking.test;
  */
 import static org.junit.Assert.fail;
 
-import java.util.ArrayList;
-
 import junit.framework.TestCase;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.example.easycooking.model.Image;
-import com.example.easycooking.model.Ingredient;
 import com.example.easycooking.model.Recipe;
 import com.example.easycooking.model.Step;
 public class recipetest extends TestCase {
 	
-	
+	     private Recipe recipe;
+      
+			@Before
+				   public void setup() throws Exception {
+					 recipe = new Recipe();
+					
+				}
+			@After 
+				   public void teardown() throws Exception{
+					 recipe = null;					  
+				}
+		
 
+			// Test ID can not BE NULL
+			 @Test
+				 public void testgetID(){		    	   				     
+						assertNull(recipe.getID());
+				   }
 
-	// Test ID can not BE NULL
-       @Test
-	 public void testgetID(){
-		    Recipe recipe = new Recipe();  
-			assertNull(recipe.getID());
-	  }
-    // Test setName of the recipe
-			@Test
-			public void testgetName()
-			{
-		        Recipe recipe = new Recipe();  
-		        recipe.setName("pizza");  
-		        assertEquals("Name", "pizza", recipe.getName());
-			}
-	//Test set_download_upload_own and get_download_upload_own
-           @Test
-             public void testget_download_upload_own(){
-        	   Recipe recipe = new Recipe();  
-        	   recipe.set_download_upload_own(0);
-        	   assertEquals(0, recipe.get_download_upload_own());        	   
-           }
-   //Test if the getStep runs
-            @Test
-             public void testgetSteps(){
-        	
-        	   java.util.ArrayList<Ingredient>  v1 = new java.util.ArrayList<Ingredient>();
-				java.util.ArrayList<Image>  v2 = new java.util.ArrayList<Image>();
-				Step step = new Step(1,"112233","make it");
-				Recipe recipe = new Recipe("12345","pizza",v2,v1,step,0);         
-        	   assertNotNull(recipe.getSteps());      	   
-           }
-    //Test if there is no image
-           @Test
-             public void testgetImages(){  
-				java.util.ArrayList<Ingredient>  v1 = new java.util.ArrayList<Ingredient>();
-				java.util.ArrayList<Image>  v2 = new java.util.ArrayList<Image>();
-				Step step = new Step();
-       		    Recipe recipe = new Recipe("11111","qq",v2,v1,step,0);              
-       	      recipe.setSteps(step);
-        	   assertNotNull(recipe.getImages());      	   
-           }
- 
-           
-   //Test setSteps
-           @Test
-             public void teststeps(){
-				java.util.ArrayList<Ingredient>  v1 = new java.util.ArrayList<Ingredient>();
-				java.util.ArrayList<Image>  v2 = new java.util.ArrayList<Image>();
-				Step step = new Step(1,"12345","test");
-        		Recipe recipe = new Recipe("12345","pizza",v2,v1,step,0);              
-        	   recipe.setSteps(step);
-        	   assertNotNull(recipe.getSteps());                     
-            }
-         
-     //Test setimages
-           @Test
-             public void testimages(){
-				java.util.ArrayList<Ingredient>  v1 = new java.util.ArrayList<Ingredient>();
-				java.util.ArrayList<Image>  v2 = new java.util.ArrayList<Image>();
-				Step step = new Step();
-       		Recipe recipe = new Recipe("12345","pizza",v2,v1,step,0);              
-       	   recipe.setImages(v2);
-       	   assertNotNull(recipe.getImages());                   
-           }    
-    
-           
- 
-           
-           //test share recipe  
-       	@Test
-    	public void sharetest() {
-    		fail("Not yet implemented");
-    	}
-
-       	
-       	
-       	//test publish and download a recipe
-    	@Test
-    	public void webservertest() {
-    		fail("Not yet implemented");
-    	}
-
-    	
-    	
-    	// test search recipe	
-    	public void searchtest() {
-    		java.util.ArrayList<Ingredient>  v1 = new java.util.ArrayList<Ingredient>();
-			java.util.ArrayList<Image>  v2 = new java.util.ArrayList<Image>();
-			Step step = new Step();
-			Recipe recipe = new Recipe("54321","soup",v2,v1,step,0);            
-    	}
-    	
-    	
-    	
-    	
-}		
-  
-	
+		    // Test setName of the recipe
+	              
+			 @Test
+					public void testgetName(){				         
+				        recipe.setName("pizza");
+				        String expect = "pizza";
+				        assertEquals(expect, recipe.getName());
+					}
+		     
+		   //Test set_download_upload_own and get_download_upload_own
+	          
+			  @Test
+	               public void testget_download_upload_own(){
+	         
+	        	   recipe.set_download_upload_own(0);
+	        	   assertEquals(0, recipe.get_download_upload_own());        	   
+	           }
+	        
+			//Test if the getStep runs
+	          
+			  @Test
+	              public void testgetSteps(){					
+					Step step = new Step(1,"112233","make it");
+					recipe.setSteps(step);					       
+	        	   assertEquals(step,recipe.getSteps());      	   
+	           }
+	       //Test if there is no image
+	           @Test
+	              public void testgetImages(){  
+					java.util.ArrayList<Image>  v2 = new java.util.ArrayList<Image>();
+					recipe.setImages(v2);
+					assertNotNull(recipe.getImages());      	   
+	           }
+	 
+	           
+	      //Test setSteps
+	           @Test
+	              public void teststeps(){
+					Step step = new Step(1,"12345","test");              
+	        	    recipe.setSteps(step);
+	        	    assertNotNull(recipe.getSteps());                     
+	            }
+	    
+	           
+	    	
+	    	
+	    	
+	}		
