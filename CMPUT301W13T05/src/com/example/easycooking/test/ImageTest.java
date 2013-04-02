f69027eb2a4f5c512b52fcbadf37a72575b597f0
chenkun <chenkun@ualberta.ca>
2013-04-02 15:49:48 -0600

    Update ImageTest.java

--- CMPUT301W13T05/src/com/example/easycooking/test/ImageTest.java
+++ CMPUT301W13T05/src/com/example/easycooking/test/ImageTest.java
@@ -14,21 +14,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ImageTest extends TestCase{
+	
+	Image image;
+	
+	
+	
+	@Before
+	public void setUp()  {
+	         	 	
+                image = new Image("1","imagetestrid","1010101010101")
+	}
 	@Test
 	public void testGetType() {
-		Image image = new Image("1","imagetestrid", "1010101010101");
+	//	Image image = new Image("1","imagetestrid", "1010101010101");
 		assertEquals("1", image.get_IMAGE_ID());
 	}
 	@Test
 	public void testGetAmount() {
-		Image image = new Image("1","imagetestrid", "1010101010101");
+	//	Image image = new Image("1","imagetestrid", "1010101010101");
 		assertEquals("imagetestrid", image.get_image_belongto());
 	}
 	@Test
 	public void testGetUnit() {
-		Image image = new Image("1","imagetestrid", "1010101010101");
+	//	Image image = new Image("1","imagetestrid", "1010101010101");
 		assertEquals("1010101010101", image.get_imageUri());
 	}
+        @After
+        public void tearDown(
+        	image = null;					 
+        }
+
 	
 }	
 	
